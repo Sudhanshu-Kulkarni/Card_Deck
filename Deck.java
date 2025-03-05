8057679d8e4b844970d9a2f27e8a7bc4a09adc49
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-03-05 19:26:15 +0530

    Added method to create deck of 52 cards

--- Deck.java
+++ Deck.java
@@ -11,4 +11,16 @@ public class Deck {
 		createDeck();
 	}
 
+	// Method to create a deck of 52 cards
+	private void createDeck() {
+		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+		String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
+
+		for (String suit : suits) {
+			for (String rank : ranks) {
+				deck.add(new Card(rank, suit));
+			}
+		}
+	}
+
 }
