e125c526c547184e17b0eef4f46b61aedee9481c
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-03-05 20:14:05 +0530

    Added method to print and  get FirstCard

--- Deck.java
+++ Deck.java
@@ -11,6 +11,12 @@ public class Deck {
 		createDeck();
 	}
 
+	public Card getFirstCard() {
+		shuffleDeck(); // Shuffle first
+		return deck.getFirst(); // Get first card
+	}
+
+
 	// Method to create a deck of 52 cards
 	private void createDeck() {
 		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
