df3e9da77e64ee55027544a5442988f08c7f2d47
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-03-05 19:10:31 +0530

    Initial Commit. Created Card.java

---
+++ Card.java
@@ -0,0 +1,13 @@
+// Card.java
+
+public class Card {
+	private String rank;
+	private String suit;
+
+	// Constructor
+	public Card(String rank, String suit) {
+		this.rank = rank;
+		this.suit = suit;
+	}
+
+}
